3b5e1cfd7500d95b8fc64f00c512f2278c91cc91
Barbapapazes <e.soubiran25@gmail.com>
2023-02-12 17:12:25 +0100

    feat: add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['@classement-des-associations/website-theme']
+  extends: ['@classement-des-associations/website-theme'],
+
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', 'type': 'image/png', href: '/favicon.png' },
+      ],
+    },
+  },
 })
