063742d18966db507bb1909a873f6b6288314883
Barbapapazes <e.soubiran25@gmail.com>
2023-02-12 12:32:27 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <BaseSection>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa itaque tempore sit provident ipsum corrupti enim quo natus. Enim, dolore totam repudiandae ab repellat illo nulla quasi repellendus maiores velit.
+  </BaseSection>
+</template>
