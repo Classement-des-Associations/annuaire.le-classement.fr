344f9ab3c6116b7828149402e25c36b87d595d8f
Barbapapazes <e.soubiran25@gmail.com>
2023-03-03 10:47:45 +0100

    fix: update about page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
+      contributeFormLink: 'https://example.com',
       newsletterSubscriptionLink: 'https://example.com',
       trailingSlash: true,
       titleSeparator: '·',
