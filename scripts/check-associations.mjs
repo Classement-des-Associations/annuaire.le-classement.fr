0767e87bc4710bea1d3896e63302da26f8672e6b
Barbapapazes <e.soubiran25@gmail.com>
2023-03-03 11:26:02 +0100

    chore: add log to scripts

--- scripts/check-associations.mjs
+++ scripts/check-associations.mjs
@@ -6,6 +6,7 @@ function main () {
   // This script is used to check which images are not used by any association
   // Associations are stored in `/content/associations-etudiantes/data/*.json`
   // Images are stored in `/public/assets/associations/images/*.png`
+  consola.start('Checking if each image is used by an association...')
 
   const associationsPath = resolve('./content/associations-etudiantes/data')
 
@@ -15,13 +16,17 @@ function main () {
   // Read all the images
   const images = fs.readdirSync('./public/assets/associations/images')
 
+  let linked = 0
   // Check if all the images are used by an association
   for (const image of images) {
     const name = image.replace('.png', '')
     if (!associations.includes(`_${name}.json`)) {
+      linked++
       consola.warn(`Unused image ${name} (${resolve('./public/assets/associations/images', image)})`)
     }
   }
+
+  if (linked === 0) { consola.success('Each image is used by an association 🎉') } else { consola.fatal(`${linked} images are not used by any association`) }
 }
 
 main()
--- scripts/check-images.mjs
+++ scripts/check-images.mjs
@@ -1,11 +1,12 @@
 import fs from 'node:fs'
-import { resolve } from 'pathe'
 import consola from 'consola'
+import { resolve } from 'pathe'
 
 function main () {
   // This script is used to check which associations don't have images
   // Associations are stored in `/content/associations-etudiantes/data/*.json`
   // Images are stored in `/public/assets/associations/images/*.png`
+  consola.start('Checking if each association has an image...')
 
   const associationsPath = resolve('./content/associations-etudiantes/data')
 
@@ -15,13 +16,17 @@ function main () {
   // Read all the images
   const images = fs.readdirSync('./public/assets/associations/images')
 
+  let linked = 0
   // Check if all the associations have an image
   for (const association of associations) {
     const name = association.replace('.json', '').replace(/_/g, '')
     if (!images.includes(`${name}.png`)) {
+      linked++
       consola.warn(`Missing image for ${name} (${resolve(associationsPath, association)})`)
     }
   }
+
+  if (linked === 0) { consola.success('Each association have an image 🎉') } else { consola.fatal(`${linked} associations does not have an image`) }
 }
 
 main()
