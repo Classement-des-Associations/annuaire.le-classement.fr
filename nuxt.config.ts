d47167b20a9f55e69f4f508e58b2d8f9d6c68389
Barbapapazes <e.soubiran25@gmail.com>
2023-03-03 20:08:45 +0100

    feat: add clarity module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ export default defineNuxtConfig({
   extends: ['@classement-des-associations/website-theme'],
 
   modules: [
-    '@vueuse/nuxt'
+    '@vueuse/nuxt',
+    'nuxt-clarity-analytics'
   ],
 
   runtimeConfig: {
